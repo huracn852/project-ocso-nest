650d87a74313de4dc98ab6a235b5efc8d45d796b
huracn852 <mata.reyes.andres.david@gmail.com>
2026-09-13 23:02:28 -0600

    Entity for employees

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -2,58 +2,46 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
 import {v4 as uuid} from 'uuid';
+import { Employee } from './entities/employee.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class EmployeesService {
-  private employees: CreateEmployeeDto[] = [
-{
-  id: uuid(),
-  name: "Alberto",
-  lastName: "Costa",
-  phoneNumber: "123456789"
-},
-{
-  id: uuid(),
-  name: "Jose",
-  lastName: "Perez",
-  phoneNumber: "987654321",
-}]
-  create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = uuid()
-    this.employees.push(createEmployeeDto);
-    return createEmployeeDto;
+  constructor(
+    @InjectRepository(Employee)
+    private employeeRepository: Repository<Employee>
+  ){}
+  async create(createEmployeeDto: CreateEmployeeDto) {
+    const employee = await this.employeeRepository.save(createEmployeeDto);
+    return employee;
+ 
   }
 
   findAll() {
-    return this.employees;
+    return this.employeeRepository.find();
   }
 
   findOne(id: string) {
-    const employee = this.employees.filter(employee => employee.id === id)[0];
-    if(!employee)
-    throw new NotFoundException();
+    const employee = this.employeeRepository.findOneBy({employeeId: id});
     return employee;
   }
 
-  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
-    let employeeToUpdate = this.findOne(id);
-    employeeToUpdate = {
-      ...employeeToUpdate,
-      ...updateEmployeeDto,
+  async update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
+    const employeeToUpdate = await this.employeeRepository.preload({
+      employeeId: id, ...updateEmployeeDto
+    });
+
+    if (!employeeToUpdate) {
+      throw new NotFoundException(`Empleado con id ${id} no encontrado`);
     }
-    this.employees = this.employees.map((employee) => {
-      if (employee.id === id) {
-        employee = employeeToUpdate
-      }
-      return employee
-    })
-    
-    return employeeToUpdate;
+
+    return this.employeeRepository.save(employeeToUpdate);
   }
 
   remove(id: string) {
-    this.findOne(id);
-    this.employees = this.employees.filter(employee => employee.id !== id);
-    return this.employees;
+    this.employeeRepository.delete({employeeId: id});
+    return {message: `Empleado eliminado correctamente`};
   }
 }
+
--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,1 +1,19 @@
-export class Employee {}
+import { text } from 'stream/consumers';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Employee {
+  @PrimaryGeneratedColumn('uuid')
+  employeeId: string;
+
+  @Column('text')
+  name: string;
+
+  @Column('text')
+  lastName: string;
+
+  @Column('text')
+  phoneNumber: string;
+    @Column('text')
+    email: string;
+}
